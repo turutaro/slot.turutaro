6f641deb9585d755ce42e498bf326c073f700479
turutaro <63895605+turutaro@users.noreply.github.com>
2020-09-10 16:31:47 +0900

    Update slot.js

--- slot.js
+++ slot.js
@@ -1,5 +1,4 @@
 'use strict' 
-
 {
     class Panel {
         constructor() {
@@ -62,10 +61,30 @@
             this.img.classList.add('unmatched');
         }
 
+        matched(p1, p2) {
+            return this.img.src === p1.img.src && this.img.src === p2.img.src;
+        }
+
         activate() {
             this.img.classList.remove('unmatched');
             this.stop.classList.remove('inactive');
         }
+    }
+
+    function checkSrc(panel) {
+        const getCoins = document.getElementById('get-coins');
+        if (panel.img.src === 'file:///Users/tsuruokatakayoshi/Documents/dotinstall/slot/img/seven.png') {
+            coins.textContent = nowCoins += 10;
+            getCoins.textContent = 10;
+        }
+        if (panel.img.src === 'file:///Users/tsuruokatakayoshi/Documents/dotinstall/slot/img/bell.png') {
+            coins.textContent = nowCoins += 5;
+            getCoins.textContent = 5;
+        }
+        if (panel.img.src === 'file:///Users/tsuruokatakayoshi/Documents/dotinstall/slot/img/cherry.png') {
+            coins.textContent = nowCoins += 3;
+            getCoins.textContent = 3;
+        }      
     }
 
     function checkResult() {
@@ -77,8 +96,38 @@
         }
         if (panels[2].isUnmatched(panels[0], panels[1])) {
             panels[2].unmatch();
+        } 
+        if (panels[0].matched(panels[1], panels[2])) {
+            clear.classList.remove('hidden');
+            checkSrc(panels[0]);
         }
     }
+
+    function charge() {
+        const charge = confirm('コインをチャージしますか？');
+        if (charge) {
+            nowCoins = 30;
+            coins.textContent = nowCoins;
+            spin.classList.remove('inactive');
+        } else {
+            const div = document.createElement('div');
+            div.textContent = 'GAME OVER';
+            div.classList.add('gameover');
+            const body = document.querySelector('body');
+            body.appendChild(div);
+        }
+    }
+
+    function useCoin() {
+        coins.textContent = --nowCoins;
+        i++;
+        let timeoutId = setTimeout(() => {
+            useCoin();
+        },80);
+        if (i === 3) {
+            clearTimeout(timeoutId);
+        }
+        }
 
     const panels = [
         new Panel(),
@@ -87,13 +136,30 @@
     ];
 
     let panelsLeft = 3;
+    
+    const clear = document.getElementById('clear');
+
+    let i = 0;
+    let nowCoins = 3;
+    const coins = document.getElementById('now-coins');
+    coins.textContent = nowCoins;
+    
 
     const spin = document.getElementById('spin');
     spin.addEventListener('click', () => {
         if (spin.classList.contains('inactive')) {
             return;
         }
+        clear.classList.add('hidden');
         spin.classList.add('inactive');
+
+        if (nowCoins < 3) {
+            charge();
+            return;
+        }
+
+        i = 0;
+        useCoin();
 
         panels.forEach(panel => {
             panel.activate();
